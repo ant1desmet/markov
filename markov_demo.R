cf72db186d37581d0857a34eac1db0be15a83f2d
Antoine Desmet <antoine@ant-1.fr>
2020-07-14 07:20:01 +1000

    initial commit

---
+++ markov_demo.R
@@ -0,0 +1,3 @@
+#Probablikity of being picked - sample with replacement, 1 out of 100 balls in urn
+T <- matrix(c(0.99, 0.01, 0, 0), nrow = 2, byrow = T, dimnames = list(c("u","d"),c("U","D")))
+S <- nam
