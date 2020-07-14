8ecb8edeb4ed863a9c22d3506542af18b93a27d7
Antoine Desmet <antoine@ant-1.fr>
2020-07-14 22:29:54 +1000

    built a ggplot tile for MCMC result

--- markov_demo.R
+++ markov_demo.R
@@ -1,3 +1,66 @@
-#Probablikity of being picked - sample with replacement, 1 out of 100 balls in urn
-T <- matrix(c(0.99, 0.01, 0, 0), nrow = 2, byrow = T, dimnames = list(c("u","d"),c("U","D")))
-S <- nam
+library(expm)
+library(ggplot2)
+library(plyr)
+library(reshape2)
+library(matrixStats)
+#Probability of being picked after n draws - sample with replacement, 1 out of 100 balls in urn
+Trns <- matrix(c(0.99, 0.01, 1, 0), nrow = 2, byrow = T, dimnames = list(c("u","d"),c("U","D")))
+State <- c(1,0)
+
+State%*%(Trns^100)
+
+1-pgeom(q = 99,prob = 0.01)
+#how do we do this if there's sample without replacement?
+#distribution is not stationary
+
+
+#state probabilities forecasting
+HealthTrns <- matrix(c(0.90, 0.05, 0.05, 0.0,
+                       0.00, 0.00, 0.50, 0.5,
+                       0.15, 0.25, 0.60, 0.0,
+                       0.00, 0.00, 0.00, 1.0),
+                       nrow = 4, byrow = T,
+                     dimnames = list(c("healthy", "event", "risk", "death"),
+                                     c("Healthy", "Event", "Risk", "Death")))
+HealthState <- c(1,0,0,0)
+
+HealthState%*%(HealthTrns%^%40)
+probs <- sapply(X = 1:40, function(x) HealthState%*%(HealthTrns%^%x))
+DF <- as.data.frame(cbind(1:40,t(probs)))
+names(DF) <-c("years","healthy", "event", "risk", "death")
+DF <- melt(DF, id.vars = "years")
+#ggplot(DF, aes(x=years, y=value, colour = variable))+geom_line()
+ggplot(DF, aes(x=years, y=value, fill = variable))+geom_area()
+
+
+
+#a health plan simulation with MCMC
+cf <- data.frame(Healthy=c(1000,1000), Event=c(-10000,-10000), Risk=c(1000,1000), Death=c(0,0))
+
+processRow <- function(patientState){
+  sapply(patientState, function(x)  sample(x=c(1,2,3,4), size=1, replace=TRUE, prob= HealthTrns[x,]))
+}
+
+patientStates <- matrix(rep(1,5000))
+for(i in 1:40){patientStates <- cbind(patientStates,processRow(patientStates[,i]))}
+DF <- as.data.frame(cbind(1:41,t(patientStates)))
+names(DF) <- c('yr', paste("patient",1:5000,sep = '_'))
+ggDF <- melt(DF, id.vars = 'yr')
+#ggplot(ggDF, aes(x=yr, y=variable, fill = as.factor(value)))+geom_tile()
+
+financials <- apply(patientStates, 1:2, function(x) cf[1,x])
+rowSums(financials)
+which.max(rowSums(financials))
+which.min(rowSums(financials))
+table(patientStates[which.min(rowSums(financials)),])
+cumFinancials <- rowCumsums(financials)
+
+binnedCumFinancials <- round(cumFinancials/10000)*10000
+freqs <- lapply(1:40, function(x) data.frame(yrs = x, 
+                                              financial= names(table(binnedCumFinancials[,x])), 
+                                              value=unname(table(binnedCumFinancials[,x]))
+                                              )
+               )
+densityDF <- do.call(rbind, freqs)
+densityDF$financial <- as.numeric(as.character(densityDF$financial))
+ggplot(densityDF, aes(x=yrs,y=financial,fill=value.Freq))+geom_tile()
